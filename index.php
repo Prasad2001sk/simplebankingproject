5eced070cd795416f95f68bf6ecf780367ce63b3
Prasad Kulkarni <64323296+Prasad2001sk@users.noreply.github.com>
2021-02-01 19:02:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+ <html class="no-js"> 
+    <head>
+        <meta charset="utf-8">
+        <title>PSK BANK</title>
+    	<meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="bootstrap.min.css">
+        
+           
+        
+        <link rel="stylesheet" href="templatemo_style.css">
+                
+
+        
+    </head>
+    <body>
+
+
+        <div class="site-main" id="sTop">
+            <div class="site-header">
+                <div class="main-header">
+                    <div class="container">
+                        <div id="menu-wrapper">
+                            <div class="row">
+                                <nav class="navbar navbar-inverse" role="navigation">
+                                    <div class="navbar-header">
+                                        <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                        </button>
+                                        <a href="#top" class="navbar-brand">PSK BANk</a>
+                                        <div id="main-nav" class="collapse navbar-collapse">
+                                            <ul class="menu-first nav navbar-nav" style="margin-right: 20px;">
+                                                <li class="active"><a href="#">Home</a></li>
+                                                <li><a href="#about">About Us</a></li>
+                                                <li><a href="#contact-us">Contact</a></li>                                 
+                                            </ul>                                    
+                                        </div> <!-- /.main-menu -->
+                                    </div>
+                                </nav>
+                            </div> <!-- /.row -->
+                        </div> <!-- /#menu-wrapper -->                        
+                    </div> <!-- /.container -->
+                </div> <!-- /.main-header -->
+            </div> <!-- /.site-header -->
+        </div> <!-- /.site-main -->
+
+
+        <div class="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="banner-content">
+                            <h6>Simple Banking System</h6>
+                            <h2>The Spark Foundation</h2>
+                            <ul class="buttons">
+                                <li>
+                                    <div class="primary-button">
+                                        <a href="viewusers.php">View User</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="secondary-button">
+                                        <a href="transfer.php">Transfer Money</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="third-button">
+                                        <a href="transferdetail.php">Transaction History</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="intro">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4>Explore Exclusive Offer on car loand and Home loan With PSK BANK</h4>
+                        <h6>Apply For Home Loan & Get Benifit!</h6>
+                        <div class="border-button"><a href="#What we can do else">Discover More</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        
+        <div class="second-tabs-content what-we-do">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-heading">
+                            <h4>What we can do else</h4>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="wrapper">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <ul class="tabs clearfix" data-tabgroup="second-tab-group">
+                                    <li class="col-md-15 col-sm-12 col-xs-12">
+                                        <a href="#unique" class="active">
+                                            <div class="list-item">
+                                                <div class="icon">
+                                                    <img style="width: 100px; height: 10;" src="img/3.jpg" alt="">
+                                                </div>
+                                                <h4>Gold Loan</h4>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="col-md-15 col-sm-12 col-xs-12">
+                                        <a href="#theme">
+                                            <div class="list-item">
+                                                <div class="icon">
+                                                    <img style="width: 100px; height: 10;" src="img/7.png" alt="">
+                                                </div>
+                                                <h4>Personal Loan</h4>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="col-md-15 col-sm-12 col-xs-12">
+                                        <a href="#translation">
+                                            <div class="list-item">
+                                                <div class="icon">
+                                                    <img style="width: 100px; height: 10;" src="img/4.png" alt="">
+                                                </div>
+                                                <h4>Car Loan</h4>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="col-md-15 col-sm-12 col-xs-12">
+                                        <a href="#event">
+                                            <div class="list-item">
+                                                <div class="icon">
+                                                    <img style="width: 100px; height: 10;" src="img/5.jpg" alt="">
+                                                </div>
+                                                <h4>Home Loan</h4>
+                                            </div>
+                                        </a>
+                                    </li>
+                                    <li class="col-md-15 col-sm-12 col-xs-12">
+                                        <a href="#photo">
+                                            <div class="list-item">
+                                                <div class="icon">
+                                                    <img style="width: 100px;height: 10;" src="img/6.jpg" alt="">
+                                                </div>
+                                                <h4>Fix Deposit</h4>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-12">
+                            <p>Copyright &copy; 2021 PSK BANK <br> 
+                            </p>
+                    </div>
+                    <div class="col-md-4 col-sm-12">
+                        <ul class="social-icons">
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Linkedin</a></li>
+                            <li><a href="#">Instagram</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-md-offset-2 col-sm-12">
+                        <div class="back-to-top">
+                            <a href="#top">
+                                <i class="fa fa-angle-up"></i>
+                                back to top
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+        <script src="js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+       
+        <script type="text/javascript">
+        $(document).ready(function() {
+            
+            // mobile nav toggle
+            $('#nav-toggle').on('click', function (event) {
+                event.preventDefault();
+                $('#main-nav').toggleClass("open");
+            });
+        });
+        </script>
+        
+    </body>
+</html>
